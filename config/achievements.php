bd15aef68c12ff90b6d6533b7f39cf76f90c686c
Bostigger <tricksteck@gmail.com>
2023-12-24 22:36:26 +0000

    Add custom configuration settings

---
+++ config/achievements.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+    'lessons_watched' => [
+        'FirstLessonWatched' => 1,
+        'FiveLessonsWatched' => 5,
+        'TenLessonsWatched' => 10,
+        'TwentyFiveLessonsWatched' => 25,
+        'FiftyLessonsWatched' => 50,
+    ],
+    'comments_written' => [
+        'FirstCommentWritten' => 1,
+        'ThreeCommentsWritten' => 3,
+        'FiveCommentsWritten' => 5,
+        'TenCommentsWritten' => 10,
+        'TwentyCommentsWritten' => 20,
+    ],
+];
+
