037941fadfec20bd5f88516e5d4262765ac82618
Bostigger <tricksteck@gmail.com>
2023-12-24 23:09:10 +0000

    Implement controller logic for user achievements

--- app/Http/Controllers/AchievementsController.php
+++ app/Http/Controllers/AchievementsController.php
@@ -3,18 +3,41 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use App\Services\AchievementService;
 use Illuminate\Http\Request;
 
 class AchievementsController extends Controller
 {
+    protected AchievementService $achievementService;
+
+    public function __construct(AchievementService $achievementService)
+    {
+        $this->achievementService = $achievementService;
+    }
+
+    /**
+     * @throws \Exception
+     */
     public function index(User $user)
     {
+        // Calculate the unlocked achievements
+        $unlockedAchievements = $this->achievementService->getUnlockedAchievements($user);
+
+        // Calculate the next available achievements
+        $nextAvailableAchievements = $this->achievementService->getNextAvailableAchievements($user);
+
+        // Get the current badge
+        $currentBadge = $this->achievementService->getCurrentBadge($user);
+
+        // Determine the next badge and remaining achievements for it
+        [$nextBadge, $remainingToUnlockNextBadge] = $this->achievementService->getNextBadgeInfo($user);
+
         return response()->json([
-            'unlocked_achievements' => [],
-            'next_available_achievements' => [],
-            'current_badge' => '',
-            'next_badge' => '',
-            'remaing_to_unlock_next_badge' => 0
+            'unlocked_achievements' => $unlockedAchievements,
+            'next_available_achievements' => $nextAvailableAchievements,
+            'current_badge' => $currentBadge,
+            'next_badge' => $nextBadge,
+            'remaining_to_unlock_next_badge' => $remainingToUnlockNextBadge
         ]);
     }
 }
